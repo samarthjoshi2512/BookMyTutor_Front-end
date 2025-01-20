87b3d5ab0b41be606a0737df4cd42b99a87bd286
Samarth Joshi <samarthjoshi81@gmail.com>
2025-01-20 20:09:48 +0530

    fix some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {Route, Routes} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Teachers from './pages/Teachers'
 import Login from './pages/Login'
 import About from './pages/About'
